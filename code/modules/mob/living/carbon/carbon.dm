xXCrimemanXx/Scarlet-Reach2

[PORT] [FEAT] Add Dullahan species (#468)
* Ports https://github.com/OddsMachine/Roguedev/pull/1

* Adds https://github.com/OddsMachine/Roguedev/pull/3

Only change from the PR is I gave dullahans demihuman customization.

* quick fix so you can emote from your head

* fixes simplemobs not dying from decapitation

* Fixes grabs not breaking when dullahan removes own head

* fixed emote colors for other people

* Dullahan runtime fixes

* Merge remote-tracking branch 'upstream/main' into i-love-miss-fortune-skullgirls

* Github Desktop was going crazy otherwise

* misc fixes

* heads can now go on belts

i have been asked to instead add this to all heads

* Merge conflict reverted this file entirely..

* i have been asked to add plain bodymarkings

* MAKES SURE its only the head

for some reason you couldn't attach legs but you could attach arms?? so weird anyways theres the bugfix

---------

Co-authored-by: OddsMachine <221254306+OddsMachine@users.noreply.github.com>

code/modules/mob/living/carbon/carbon.dm
@@ -602,6 +602,15 @@
 
 	mob_timers["puke"] = world.time
 
+	var/obj/item/bodypart/head/dullahan/vomitrelay
+	if(isdullahan(src))
+		var/mob/living/carbon/human = src
+		var/datum/species/dullahan/dullahan = human.dna.species
+		if(dullahan.headless)
+			vomitrelay = dullahan.my_head
+
+	var/atom/movable/vomit_source = vomitrelay ? vomitrelay : src
+
 	if(nutrition <= 50 && !blood)
 		if(message)
 			emote("gag")
@@ -613,7 +622,18 @@
 			return TRUE
 		add_nausea(-100)
 		energy_add(-50)
-		if(is_mouth_covered()) //make this add a blood/vomit overlay later it'll be hilarious
+		if(vomitrelay && ishuman(vomitrelay.loc))
+			var/mob/living/carbon/human/parent = vomitrelay.loc
+			if(message)
+				visible_message("<span class='danger'>[vomitrelay] throws up all over [parent]!</span>", \
+								"<span class='danger'>I puke all over [parent]!</span>")
+				SEND_SIGNAL(parent, COMSIG_ADD_MOOD_EVENT, "vomitother", /datum/mood_event/vomitother)
+				parent.add_stress(/datum/stressevent/vomitother)
+
+				SEND_SIGNAL(src, COMSIG_ADD_MOOD_EVENT, "vomitedonother", /datum/mood_event/vomitedonother)
+				src.add_stress(/datum/stressevent/vomitedonother)
+			distance = 0
+		else if(is_mouth_covered()) //make this add a blood/vomit overlay later it'll be hilarious
 			if(message)
 				visible_message("<span class='danger'>[src] throws up all over [p_them()]self!</span>", \
 								"<span class='danger'>I puke all over myself!</span>")
@@ -624,7 +644,7 @@
 			distance = 0
 		else
 			if(message)
-				visible_message("<span class='danger'>[src] pukes!</span>", "<span class='danger'>I puke!</span>")
+				visible_message("<span class='danger'>[vomit_source] pukes!</span>", "<span class='danger'>I puke!</span>")
 				SEND_SIGNAL(src, COMSIG_ADD_MOOD_EVENT, "vomit", /datum/mood_event/vomit)
 				if(iscarbon(src))
 					var/mob/living/carbon/C = src
@@ -633,20 +653,20 @@
 		if(NOBLOOD in dna?.species?.species_traits)
 			return TRUE
 		if(message)
-			visible_message("<span class='danger'>[src] coughs up blood!</span>", "<span class='danger'>I cough up blood!</span>")
+			visible_message("<span class='danger'>[vomit_source] coughs up blood!</span>", "<span class='danger'>I cough up blood!</span>")
 
 	if(stun)
 		Immobilize(59)
 
+	var/turf/T = get_turf(vomit_source)
 	if(!blood)
-		playsound(get_turf(src), pick('sound/vo/vomit.ogg','sound/vo/vomit_2.ogg'), 100, TRUE)
+		playsound(T, pick('sound/vo/vomit.ogg','sound/vo/vomit_2.ogg'), 100, TRUE)
 	else
 		if(stat != DEAD)
-			playsound(src, pick('sound/vo/throat.ogg','sound/vo/throat2.ogg','sound/vo/throat3.ogg'), 100, FALSE)
+			playsound(vomit_source, pick('sound/vo/throat.ogg','sound/vo/throat2.ogg','sound/vo/throat3.ogg'), 100, FALSE)
 
 	blur_eyes(10)
 
-	var/turf/T = get_turf(src)
 	if(!blood)
 		if(nutrition > 50)
 			adjust_nutrition(-lost_nutrition)
@@ -657,7 +677,25 @@
 	for(var/i=0 to distance)
 		if(blood)
 			if(T)
-				bleed(5)
+				if(vomitrelay && blood_volume > 0)
+					var/mob/living/carbon/human/parent = vomitrelay.loc
+					var/amt = 5 * parent.physiology.bleed_mod
+					blood_volume = max(blood_volume - amt, 0)
+					GLOB.scarlet_round_stats[STATS_BLOOD_SPILT] += amt
+					if(isturf(vomit_source.loc))
+						add_drip_floor(vomit_source.loc, amt)
+					var/vol2use
+					if(amt > 1)
+						var/index = min(amt - 1, 3)
+						vol2use = "sound/misc/bleed ([index]).ogg"
+					if(!(mobility_flags & MOBILITY_STAND))
+						vol2use = null
+					if(vol2use)
+						playsound(T, vol2use, 100, FALSE)
+
+					updatehealth()
+				else
+					bleed(5)
 		else if(src.reagents.has_reagent(/datum/reagent/consumable/ethanol/blazaam, needs_metabolizing = TRUE))
 			if(T)
 				T.add_vomit_floor(src, VOMIT_PURPLE)

wow50000/Lupine-Pass

code/modules/mob/living/carbon/carbon.dm
@@ -606,6 +606,15 @@
 
 	mob_timers["puke"] = world.time
 
+	var/obj/item/bodypart/head/dullahan/vomitrelay
+	if(isdullahan(src))
+		var/mob/living/carbon/human = src
+		var/datum/species/dullahan/dullahan = human.dna.species
+		if(dullahan.headless)
+			vomitrelay = dullahan.my_head
+
+	var/atom/movable/vomit_source = vomitrelay ? vomitrelay : src
+
 	if(nutrition <= 50 && !blood)
 		if(message)
 			emote("gag")
@@ -617,7 +626,18 @@
 			return TRUE
 		add_nausea(-100)
 		energy_add(-50)
-		if(is_mouth_covered()) //make this add a blood/vomit overlay later it'll be hilarious
+		if(vomitrelay && ishuman(vomitrelay.loc))
+			var/mob/living/carbon/human/parent = vomitrelay.loc
+			if(message)
+				visible_message("<span class='danger'>[vomitrelay] throws up all over [parent]!</span>", \
+								"<span class='danger'>I puke all over [parent]!</span>")
+				SEND_SIGNAL(parent, COMSIG_ADD_MOOD_EVENT, "vomitother", /datum/mood_event/vomitother)
+				parent.add_stress(/datum/stressevent/vomitother)
+
+				SEND_SIGNAL(src, COMSIG_ADD_MOOD_EVENT, "vomitedonother", /datum/mood_event/vomitedonother)
+				src.add_stress(/datum/stressevent/vomitedonother)
+			distance = 0
+		else if(is_mouth_covered()) //make this add a blood/vomit overlay later it'll be hilarious
 			if(message)
 				visible_message("<span class='danger'>[src] throws up all over [p_them()]self!</span>", \
 								"<span class='danger'>I puke all over myself!</span>")
@@ -628,7 +648,7 @@
 			distance = 0
 		else
 			if(message)
-				visible_message("<span class='danger'>[src] pukes!</span>", "<span class='danger'>I puke!</span>")
+				visible_message("<span class='danger'>[vomit_source] pukes!</span>", "<span class='danger'>I puke!</span>")
 				SEND_SIGNAL(src, COMSIG_ADD_MOOD_EVENT, "vomit", /datum/mood_event/vomit)
 				if(iscarbon(src))
 					var/mob/living/carbon/C = src
@@ -637,20 +657,20 @@
 		if(NOBLOOD in dna?.species?.species_traits)
 			return TRUE
 		if(message)
-			visible_message("<span class='danger'>[src] coughs up blood!</span>", "<span class='danger'>I cough up blood!</span>")
+			visible_message("<span class='danger'>[vomit_source] coughs up blood!</span>", "<span class='danger'>I cough up blood!</span>")
 
 	if(stun)
 		Immobilize(59)
 
+	var/turf/T = get_turf(vomit_source)
 	if(!blood)
-		playsound(get_turf(src), pick('sound/vo/vomit.ogg','sound/vo/vomit_2.ogg'), 100, TRUE)
+		playsound(T, pick('sound/vo/vomit.ogg','sound/vo/vomit_2.ogg'), 100, TRUE)
 	else
 		if(stat != DEAD)
-			playsound(src, pick('sound/vo/throat.ogg','sound/vo/throat2.ogg','sound/vo/throat3.ogg'), 100, FALSE)
+			playsound(vomit_source, pick('sound/vo/throat.ogg','sound/vo/throat2.ogg','sound/vo/throat3.ogg'), 100, FALSE)
 
 	blur_eyes(10)
 
-	var/turf/T = get_turf(src)
 	if(!blood)
 		if(nutrition > 50)
 			adjust_nutrition(-lost_nutrition)
@@ -661,7 +681,25 @@
 	for(var/i=0 to distance)
 		if(blood)
 			if(T)
-				bleed(5)
+				if(vomitrelay && blood_volume > 0)
+					var/mob/living/carbon/human/parent = vomitrelay.loc
+					var/amt = 5 * parent.physiology.bleed_mod
+					blood_volume = max(blood_volume - amt, 0)
+					GLOB.azure_round_stats[STATS_BLOOD_SPILT] += amt
+					if(isturf(vomit_source.loc))
+						add_drip_floor(vomit_source.loc, amt)
+					var/vol2use
+					if(amt > 1)
+						var/index = min(amt - 1, 3)
+						vol2use = "sound/misc/bleed ([index]).ogg"
+					if(!(mobility_flags & MOBILITY_STAND))
+						vol2use = null
+					if(vol2use)
+						playsound(T, vol2use, 100, FALSE)
+
+					updatehealth()
+				else
+					bleed(5)
 		else if(src.reagents.has_reagent(/datum/reagent/consumable/ethanol/blazaam, needs_metabolizing = TRUE))
 			if(T)
 				T.add_vomit_floor(src, VOMIT_PURPLE)