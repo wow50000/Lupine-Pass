xXCrimemanXx/Scarlet-Reach2

Dolls v2 reupload (Credit goes to viertel in discord for the gaggar text) (#477)
* VerminFolk get same roles as WildKin

About The Pull Request

Seen someone make some fair points so I pushed the PR, Tested it so it shouldn't blow up the server.

Testing Evidence

"C:\Test.1.png" "C:\Test.2.png" "C:\Test.3.png"

Why It's Good For The Game

"Wildkin existing already makes the fact that vermin aren't tolerated in those roles very silly. We already have rat people in the garrison, we have rat people as priests, hell some of the zardmen have been even more ridiculous than some of the rats. As long as wildkin exists, they will be better rats than normal rats if you wanna be multiple jobs. Also, verminvolk doesn't even necessarily mean rat people. It can be gnomes, bunnies like karmen, and other non-rat races. We had them as councilors and such before with little issue"

* Dolls

Adds dolls as a race.

* Update mobs.dm

Deleted redundant ==

* Doll V2

changes to allow limb removal for dolls

* Update grabbing.dm

Doll Update

* Update Graggar.dm

Graggar Warning

* Update grabbing.dm

Force Push fucked up this one(Fixed)

* Update doll.dm

Force push reverted my goddamn no metabolism trait addition.

* Update doll.dm

Had to remove speed debuff again due to force push mishap.

* Update grabbing.dm

code/__DEFINES/is_helpers.dm
@@ -94,6 +94,7 @@ GLOBAL_LIST_INIT(our_forest_sex, typecacheof(list(
 #define ishalfkin(A) (is_species(A, /datum/species/demihuman))
 #define iswildkin(A) (is_species(A, /datum/species/anthromorph))
 #define isgolemp(A) (is_species(A, /datum/species/golem/metal))	//Specified 'M' due to redefine lower
+#define isdoll(A) (is_species(A, /datum/species/golem/porcelain))
 #define isvermin(A) (is_species(A, /datum/species/anthromorphsmall))
 #define isaxian(A) (is_species(A, /datum/species/akula))
 #define isdracon(A) (is_species(A, /datum/species/dracon))

code/__DEFINES/roguetown.dm
@@ -187,7 +187,6 @@
 	/datum/species/moth,\
 	/datum/species/dracon,\
 	/datum/species/anthromorph,\
-	/datum/species/anthromorphsmall,\
 	/datum/species/demihuman,\
 	/datum/species/halforc,\
 	/datum/species/kobold,\
@@ -211,12 +210,14 @@
 
 #define RACES_MANMADE \
 	/datum/species/golem/metal,\
+	/datum/species/golem/porcelain,\
 
 #define RACES_SECOND_CLASS \
     /datum/species/vulpkanin,\
     /datum/species/lupian,\
     /datum/species/moth,\
     /datum/species/anthromorph,\
+    /datum/species/anthromorphsmall,\
     /datum/species/tabaxi,\
     /datum/species/lizardfolk,\
     /datum/species/dracon,\
@@ -226,10 +227,10 @@
 	/datum/species/halforc,\
 
 #define RACES_WIDELY_REVILED \
-    /datum/species/anthromorphsmall,\
     /datum/species/kobold,\
     /datum/species/goblinp,\
-	/datum/species/dullahan,\
+    /datum/species/anthromorphsmall,\
+  	/datum/species/dullahan,\
 
 #define RACES_NOBILITY_ELIGIBLE_UP list(RACES_NOBILITY_ELIGIBLE)
 
@@ -270,6 +271,7 @@
 	/datum/species/kobold,\
 	/datum/species/goblinp,\
 	/datum/species/golem/metal,\
+	/datum/species/golem/porcelain,\
 	/datum/species/dullahan,\
 )
 
@@ -295,6 +297,7 @@
 	/datum/species/kobold,\
 	/datum/species/goblinp,\
 	/datum/species/golem/metal,\
+	/datum/species/golem/porcelain,\
 	/datum/species/dullahan,\
 )
 // Non-dwarf non-kobold non-goblin mostly
@@ -316,6 +319,7 @@
 	/datum/species/demihuman,\
 	/datum/species/halforc,\
 	/datum/species/golem/metal,\
+	/datum/species/golem/porcelain,\
 	/datum/species/dullahan,\
 )
 // Non-elf non-dwarf non-kobold non-goblin mostly
@@ -333,6 +337,7 @@
 	/datum/species/anthromorph,\
 	/datum/species/demihuman,\
 	/datum/species/golem/metal,\
+	/datum/species/golem/porcelain,\
 	/datum/species/dullahan,\
 )
 #define ALL_CLERIC_PATRONS list(/datum/patron/divine/astrata, /datum/patron/divine/noc, /datum/patron/divine/dendor, /datum/patron/divine/necra, /datum/patron/divine/pestra, /datum/patron/divine/ravox, /datum/patron/divine/malum, /datum/patron/divine/eora) // Currently unused.

code/controllers/subsystem/storyteller.dm
@@ -1145,6 +1145,7 @@ SUBSYSTEM_DEF(gamemode)
 	GLOB.scarlet_round_stats[STATS_ALIVE_HALFKIN] = 0
 	GLOB.scarlet_round_stats[STATS_ALIVE_WILDKIN] = 0
 	GLOB.scarlet_round_stats[STATS_ALIVE_GOLEMS] = 0
+	GLOB.scarlet_round_stats[STATS_ALIVE_DOLLS] = 0
 	GLOB.scarlet_round_stats[STATS_ALIVE_VERMINFOLK] = 0
 	GLOB.scarlet_round_stats[STATS_ALIVE_DRACON] = 0
 	GLOB.scarlet_round_stats[STATS_ALIVE_AXIAN] = 0
@@ -1247,6 +1248,8 @@ SUBSYSTEM_DEF(gamemode)
 				GLOB.scarlet_round_stats[STATS_ALIVE_WILDKIN]++
 			if(isgolemp(human_mob))
 				GLOB.scarlet_round_stats[STATS_ALIVE_GOLEMS]++
+			if(isdoll(human_mob))
+				GLOB.scarlet_round_stats[STATS_ALIVE_DOLLS]++
 			if(isvermin(human_mob))
 				GLOB.scarlet_round_stats[STATS_ALIVE_VERMINFOLK]++
 			if(isdracon(human_mob))

code/modules/mob/living/carbon/human/species_types/roguetown/golem/doll.dm
@@ -1,0 +1,98 @@
+/mob/living/carbon/human/species/golem/porcelain
+	race = /datum/species/golem/porcelain
+	construct = 1
+
+/datum/species/golem/porcelain
+	name = "Doll"
+	id = "doll"
+	desc = "<b>Porcelain Doll</b><br>\
+	The pinnacle of both art and craftsmanship, originally made to provide companionship for ladies and wealthy women \
+	alike. Created to be simply toys or novelty decorations for the wealthy, they do not sleep, eat or bleed. However, \
+	due to their dark magic and heretical origin that even their stronger cousin share; They were made to be incredibly \
+	brittle as to promote their subservience and remove any chance these somber creations have of killing their masters. \
+	Over time, they were seen to prove as valuable asset and advisory role due to their intellectual prowess, it is \
+	unknown what provided them with such a gift. A master wanting more engaging conversation? A lord wanting a more \
+	efficient clerk? Regardless, who knows what them eyes made of glass truly reflect...<br> \
+	(Insomnia, No hunger, no blood.) \
+	(+2 Intelligence, +2 Perception, -2 Strength, -2 Constitution, -2 Endurance)"
+
+	construct = 1
+	skin_tone_wording = "Paint"
+	default_color = "FFFFFF"
+	species_traits = list(EYECOLOR,HAIR,FACEHAIR,LIPS,STUBBLE,OLDGREY,NOBLOOD)
+	default_features = MANDATORY_FEATURE_LIST
+	use_skintones = 1
+	possible_ages = ALL_AGES_LIST
+	skinned_type = /obj/item/ingot/steel
+	disliked_food = NONE
+	liked_food = NONE
+	inherent_traits = list(TRAIT_NOHUNGER, TRAIT_BLOODLOSS_IMMUNE, TRAIT_NOBREATH, TRAIT_NOSLEEP, TRAIT_CRITICAL_WEAKNESS, 
+	TRAIT_BEAUTIFUL, TRAIT_EASYDISMEMBER, TRAIT_LIMBATTACHMENT, TRAIT_NOMETABOLISM)
+	changesource_flags = MIRROR_BADMIN | WABBAJACK | MIRROR_MAGIC | MIRROR_PRIDE | RACE_SWAP | SLIME_EXTRACT
+	limbs_icon_m = 'icons/roguetown/mob/bodies/m/mcom.dmi'
+	limbs_icon_f = 'icons/roguetown/mob/bodies/f/fcom.dmi'
+	dam_icon = 'icons/roguetown/mob/bodies/dam/dam_male.dmi'
+	dam_icon_f = 'icons/roguetown/mob/bodies/dam/dam_female.dmi'
+	soundpack_m = /datum/voicepack/male
+	soundpack_f = /datum/voicepack/female
+	offset_features = list(
+		OFFSET_ID = list(0,1), OFFSET_GLOVES = list(0,1), OFFSET_WRISTS = list(0,1),\
+		OFFSET_CLOAK = list(0,1), OFFSET_FACEMASK = list(0,1), OFFSET_HEAD = list(0,1), \
+		OFFSET_FACE = list(0,1), OFFSET_BELT = list(0,1), OFFSET_BACK = list(0,1), \
+		OFFSET_NECK = list(0,1), OFFSET_MOUTH = list(0,1), OFFSET_PANTS = list(0,0), \
+		OFFSET_SHIRT = list(0,1), OFFSET_ARMOR = list(0,1), OFFSET_HANDS = list(0,1), OFFSET_UNDIES = list(0,1), \
+		OFFSET_ID_F = list(0,-1), OFFSET_GLOVES_F = list(0,0), OFFSET_WRISTS_F = list(0,0), OFFSET_HANDS_F = list(0,0), \
+		OFFSET_CLOAK_F = list(0,0), OFFSET_FACEMASK_F = list(0,-1), OFFSET_HEAD_F = list(0,-1), \
+		OFFSET_FACE_F = list(0,-1), OFFSET_BELT_F = list(0,0), OFFSET_BACK_F = list(0,-1), \
+		OFFSET_NECK_F = list(0,-1), OFFSET_MOUTH_F = list(0,-1), OFFSET_PANTS_F = list(0,0), \
+		OFFSET_SHIRT_F = list(0,0), OFFSET_ARMOR_F = list(0,0), OFFSET_UNDIES_F = list(0,-1), \
+		)
+	race_bonus = list(STAT_INTELLIGENCE = 2, STAT_PERCEPTION = 2, STAT_ENDURANCE = -2, STAT_STRENGTH = -2, STAT_CONSTITUTION = -2)
+	enflamed_icon = "widefire"
+	organs = list(
+		ORGAN_SLOT_BRAIN = /obj/item/organ/brain/golem,
+		ORGAN_SLOT_HEART = /obj/item/organ/heart/golem,
+		ORGAN_SLOT_LUNGS = /obj/item/organ/lungs/golem,
+		ORGAN_SLOT_EYES = /obj/item/organ/eyes/golem,
+		ORGAN_SLOT_EARS = /obj/item/organ/ears,
+		ORGAN_SLOT_TONGUE = /obj/item/organ/tongue/golem,
+		ORGAN_SLOT_LIVER = /obj/item/organ/liver/golem,
+		ORGAN_SLOT_STOMACH = /obj/item/organ/stomach/golem,
+		)
+	customizers = list(
+		/datum/customizer/organ/eyes/humanoid,
+		/datum/customizer/bodypart_feature/hair/head/humanoid,
+		/datum/customizer/bodypart_feature/accessory,
+		/datum/customizer/bodypart_feature/face_detail,
+		/datum/customizer/bodypart_feature/underwear,
+		)
+	body_marking_sets = list(
+		/datum/body_marking_set/none,
+	)
+	body_markings = list(
+	    /datum/body_marking/flushed_cheeks,
+		/datum/body_marking/eyeliner,
+		/datum/body_marking/tonage,
+		/datum/body_marking/nose,
+		/datum/body_marking/bangs,
+		/datum/body_marking/bun,
+	)
+
+/datum/species/golem/porcelain/check_roundstart_eligible()
+	return TRUE
+
+/datum/species/golem/porcelain/get_skin_list()
+	return list(
+		"LEAD" = "ffffff",
+		"SIENNA" = "a0522d",
+	)
+
+/datum/species/golem/porcelain/get_hairc_list()
+	return sortList(list(
+
+	"black - midnight" = "1d1b2b",
+
+	"red - blood" = "822b2b"
+
+	))
+

wow50000/Lupine-Pass

code/__DEFINES/is_helpers.dm
@@ -94,6 +94,7 @@ GLOBAL_LIST_INIT(our_forest_sex, typecacheof(list(
 #define ishalfkin(A) (is_species(A, /datum/species/demihuman))
 #define iswildkin(A) (is_species(A, /datum/species/anthromorph))
 #define isconstruct(A) (is_species(A, /datum/species/construct/metal))	//Specified 'M' due to redefine lower
+#define isdoll(A) (is_species(A, /datum/species/construct/porcelain))
 #define isvermin(A) (is_species(A, /datum/species/anthromorphsmall))
 #define isaxian(A) (is_species(A, /datum/species/akula))
 #define isdracon(A) (is_species(A, /datum/species/dracon))

code/__DEFINES/roguetown.dm
@@ -190,7 +190,6 @@
 	/datum/species/moth,\
 	/datum/species/dracon,\
 	/datum/species/anthromorph,\
-	/datum/species/anthromorphsmall,\
 	/datum/species/demihuman,\
 	/datum/species/halforc,\
 	/datum/species/kobold,\
@@ -218,7 +217,6 @@
 	/datum/species/anthromorph,\
 	/datum/species/demihuman,\
 
-
 #define RACES_SHUNNED \
 	/datum/species/halforc,\
 	/datum/species/anthromorphsmall,\
@@ -228,8 +226,10 @@
 #define RACES_DESPISED \
 	/datum/species/goblinp,\
 
+
 #define RACES_CONSTRUCT \
 	/datum/species/construct/metal,\
+	/datum/species/construct/porcelain,\
 
 #define RACES_ALL_KINDS list(RACES_DESPISED, RACES_SHUNNED, RACES_TOLERATED, RACES_RESPECTED, RACES_CONSTRUCT)
 
@@ -260,6 +260,8 @@
 	/datum/species/kobold,\
 	/datum/species/goblinp,\
 	/datum/species/construct/metal,\
+	/datum/species/construct/porcelain,\
+	/datum/species/dullahan,\
 )
 
 #define CLOTHED_RACES_TYPES list(\
@@ -285,6 +287,8 @@
 	/datum/species/kobold,\
 	/datum/species/goblinp,\
 	/datum/species/construct/metal,\
+	/datum/species/construct/porcelain,\
+	/datum/species/dullahan,\
 )
 // Non-dwarf non-kobold non-goblin mostly
 #define NON_DWARVEN_RACE_TYPES list(\
@@ -305,6 +309,8 @@
 	/datum/species/demihuman,\
 	/datum/species/halforc,\
 	/datum/species/construct/metal,\
+	/datum/species/construct/porcelain,\
+	/datum/species/dullahan,\
 )
 // Non-elf non-dwarf non-kobold non-goblin mostly
 #define HUMANLIKE_RACE_TYPES list(\
@@ -321,6 +327,8 @@
 	/datum/species/anthromorph,\
 	/datum/species/demihuman,\
 	/datum/species/construct/metal,\
+	/datum/species/construct/porcelain,\
+	/datum/species/dullahan,\
 )
 #define ALL_CLERIC_PATRONS list(/datum/patron/divine/astrata, /datum/patron/divine/noc, /datum/patron/divine/dendor, /datum/patron/divine/necra, /datum/patron/divine/pestra, /datum/patron/divine/ravox, /datum/patron/divine/malum, /datum/patron/divine/eora, /datum/patron/divine/undivided) // Currently unused.
 

code/controllers/subsystem/storyteller.dm
@@ -1150,6 +1150,7 @@ SUBSYSTEM_DEF(gamemode)
 	GLOB.azure_round_stats[STATS_ALIVE_HALFKIN] = 0
 	GLOB.azure_round_stats[STATS_ALIVE_WILDKIN] = 0
 	GLOB.azure_round_stats[STATS_ALIVE_CONSTRUCTS] = 0
+	GLOB.azure_round_stats[STATS_ALIVE_DOLLS] = 0
 	GLOB.azure_round_stats[STATS_ALIVE_VERMINFOLK] = 0
 	GLOB.azure_round_stats[STATS_ALIVE_DRACON] = 0
 	GLOB.azure_round_stats[STATS_ALIVE_AXIAN] = 0
@@ -1252,6 +1253,8 @@ SUBSYSTEM_DEF(gamemode)
 				GLOB.azure_round_stats[STATS_ALIVE_WILDKIN]++
 			if(isconstruct(human_mob))
 				GLOB.azure_round_stats[STATS_ALIVE_CONSTRUCTS]++
+			if(isdoll(human_mob))
+				GLOB.azure_round_stats[STATS_ALIVE_DOLLS]++
 			if(isvermin(human_mob))
 				GLOB.azure_round_stats[STATS_ALIVE_VERMINFOLK]++
 			if(isdracon(human_mob))

code/modules/mob/living/carbon/human/species_types/roguetown/construct/doll.dm
@@ -1,0 +1,98 @@
+/mob/living/carbon/human/species/construct/porcelain
+	race = /datum/species/construct/porcelain
+	construct = 1
+
+/datum/species/construct/porcelain
+	name = "Doll"
+	id = "doll"
+	desc = "<b>Porcelain Doll</b><br>\
+	The pinnacle of both art and craftsmanship, originally made to provide companionship for ladies and wealthy women \
+	alike. Created to be simply toys or novelty decorations for the wealthy, they do not sleep, eat or bleed. However, \
+	due to their dark magic and heretical origin that even their stronger cousin share; They were made to be incredibly \
+	brittle as to promote their subservience and remove any chance these somber creations have of killing their masters. \
+	Over time, they were seen to prove as valuable asset and advisory role due to their intellectual prowess, it is \
+	unknown what provided them with such a gift. A master wanting more engaging conversation? A lord wanting a more \
+	efficient clerk? Regardless, who knows what them eyes made of glass truly reflect...<br> \
+	(Insomnia, No hunger, no blood.) \
+	(+2 Intelligence, +2 Perception, -2 Strength, -2 Constitution, -2 Endurance)"
+
+	construct = 1
+	skin_tone_wording = "Paint"
+	default_color = "FFFFFF"
+	species_traits = list(EYECOLOR,HAIR,FACEHAIR,LIPS,STUBBLE,OLDGREY,NOBLOOD)
+	default_features = MANDATORY_FEATURE_LIST
+	use_skintones = 1
+	possible_ages = ALL_AGES_LIST
+	skinned_type = /obj/item/ingot/steel
+	disliked_food = NONE
+	liked_food = NONE
+	inherent_traits = list(TRAIT_NOHUNGER, TRAIT_BLOODLOSS_IMMUNE, TRAIT_NOBREATH, TRAIT_NOSLEEP, TRAIT_CRITICAL_WEAKNESS, 
+	TRAIT_BEAUTIFUL, TRAIT_EASYDISMEMBER, TRAIT_LIMBATTACHMENT, TRAIT_NOMETABOLISM)
+	changesource_flags = MIRROR_BADMIN | WABBAJACK | MIRROR_MAGIC | MIRROR_PRIDE | RACE_SWAP | SLIME_EXTRACT
+	limbs_icon_m = 'icons/roguetown/mob/bodies/m/mcom.dmi'
+	limbs_icon_f = 'icons/roguetown/mob/bodies/f/fcom.dmi'
+	dam_icon = 'icons/roguetown/mob/bodies/dam/dam_male.dmi'
+	dam_icon_f = 'icons/roguetown/mob/bodies/dam/dam_female.dmi'
+	soundpack_m = /datum/voicepack/male
+	soundpack_f = /datum/voicepack/female
+	offset_features = list(
+		OFFSET_ID = list(0,1), OFFSET_GLOVES = list(0,1), OFFSET_WRISTS = list(0,1),\
+		OFFSET_CLOAK = list(0,1), OFFSET_FACEMASK = list(0,1), OFFSET_HEAD = list(0,1), \
+		OFFSET_FACE = list(0,1), OFFSET_BELT = list(0,1), OFFSET_BACK = list(0,1), \
+		OFFSET_NECK = list(0,1), OFFSET_MOUTH = list(0,1), OFFSET_PANTS = list(0,0), \
+		OFFSET_SHIRT = list(0,1), OFFSET_ARMOR = list(0,1), OFFSET_HANDS = list(0,1), OFFSET_UNDIES = list(0,1), \
+		OFFSET_ID_F = list(0,-1), OFFSET_GLOVES_F = list(0,0), OFFSET_WRISTS_F = list(0,0), OFFSET_HANDS_F = list(0,0), \
+		OFFSET_CLOAK_F = list(0,0), OFFSET_FACEMASK_F = list(0,-1), OFFSET_HEAD_F = list(0,-1), \
+		OFFSET_FACE_F = list(0,-1), OFFSET_BELT_F = list(0,0), OFFSET_BACK_F = list(0,-1), \
+		OFFSET_NECK_F = list(0,-1), OFFSET_MOUTH_F = list(0,-1), OFFSET_PANTS_F = list(0,0), \
+		OFFSET_SHIRT_F = list(0,0), OFFSET_ARMOR_F = list(0,0), OFFSET_UNDIES_F = list(0,-1), \
+		)
+	race_bonus = list(STAT_INTELLIGENCE = 2, STAT_PERCEPTION = 2, STAT_ENDURANCE = -2, STAT_STRENGTH = -2, STAT_CONSTITUTION = -2)
+	enflamed_icon = "widefire"
+	organs = list(
+		ORGAN_SLOT_BRAIN = /obj/item/organ/brain/construct,
+		ORGAN_SLOT_HEART = /obj/item/organ/heart/construct,
+		ORGAN_SLOT_LUNGS = /obj/item/organ/lungs/construct,
+		ORGAN_SLOT_EYES = /obj/item/organ/eyes/construct,
+		ORGAN_SLOT_EARS = /obj/item/organ/ears,
+		ORGAN_SLOT_TONGUE = /obj/item/organ/tongue/construct,
+		ORGAN_SLOT_LIVER = /obj/item/organ/liver/construct,
+		ORGAN_SLOT_STOMACH = /obj/item/organ/stomach/construct,
+		)
+	customizers = list(
+		/datum/customizer/organ/eyes/humanoid,
+		/datum/customizer/bodypart_feature/hair/head/humanoid,
+		/datum/customizer/bodypart_feature/accessory,
+		/datum/customizer/bodypart_feature/face_detail,
+		/datum/customizer/bodypart_feature/underwear,
+		)
+	body_marking_sets = list(
+		/datum/body_marking_set/none,
+	)
+	body_markings = list(
+	    /datum/body_marking/flushed_cheeks,
+		/datum/body_marking/eyeliner,
+		/datum/body_marking/tonage,
+		/datum/body_marking/nose,
+		/datum/body_marking/bangs,
+		/datum/body_marking/bun,
+	)
+
+/datum/species/construct/porcelain/check_roundstart_eligible()
+	return TRUE
+
+/datum/species/construct/porcelain/get_skin_list()
+	return list(
+		"LEAD" = "ffffff",
+		"SIENNA" = "a0522d",
+	)
+
+/datum/species/construct/porcelain/get_hairc_list()
+	return sortList(list(
+
+	"black - midnight" = "1d1b2b",
+
+	"red - blood" = "822b2b"
+
+	))
+